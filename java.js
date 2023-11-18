c5734ff2a574a958dc95310a53eaaca3ba6426f8
Ujala savita <ujalasavita997@gmail.com>
2023-11-18 21:27:14 +0530

    checking prime number

---
+++ java.js
@@ -0,0 +1,12 @@
+let =N 
+if(n/2==m){
+    for(i=2;i<=m;i++){
+        if(n%i==0){
+            console.log("num is not prime")
+            break;
+            else {
+                console.log("num is prime")
+            }
+        }
+    }
+}
